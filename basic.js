70a45ecebc14043ee86d9ee1fdf62ed4c80c239e
praneeth123123 <23b81a6728@cvr.ac.in>
2025-07-30 10:27:05 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,53 @@
+let studentname="urvil";
+let studentage=20;
+let studentmarks=90;
+console.log(studentname);
+console.log(studentage);
+console.log(studentmarks);
+//var
+var studentname1="praneeth";
+var studentage1=21;
+var studentmarks1=85;
+console.log(studentname1);
+console.log(studentage1);
+console.log(studentmarks1)
+//const
+const std1="santhosh"
+const std2=22
+console.log(std1);
+console.log(std2);
+//data types prenitive
+let n1=10;
+let n2=3.14;
+let b1=true;
+let st1="hello js";
+console.log(n1);
+console.log(n2);
+console.log(b1);
+console.log(st1);
+//data types non prenitive
+// Object
+const person = {firstName:"ram", lastName:"ganesh"};
+// Array
+const cars = ["Land Cruiser", "Fortuner", "hycross"];
+// Date object:
+const date = new Date("2025-07-30");
+console.log(person);
+console.log(cars);
+console.log(date);
+
+//Assignment  operators
+let x=10;
+x+=10;
+console.log(x);``
+let y=10;
+y-=10;
+console.log(y);
+let z=10;
+z*=10;
+console.log(z);
+//Comparison operators
+let a=10;
+let b=20;
+console.log(a<b);
+console.log(a>b);
